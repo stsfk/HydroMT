d6d5e8d7b1758887877171a452195f74ef4bb59e
stsfk <yyang90@connect.hku.hk>
2021-06-21 21:09:49 +0800

    update documentation

--- plot_figure9.R
+++ plot_figure9.R
@@ -22,13 +22,13 @@ pacman::p_load(
   xgboost,
   lemon,
   ggrepel,
-  twosamples,
   scales
 )
 
 
 # data --------------------------------------------------------------------
 
+# load the assessment results with respect to each MR
 load("./mt_results/mr1_mt.Rda")
 p_eval_grid <- eval_grid
 
@@ -38,6 +38,9 @@ pet_eval_grid <- eval_grid
 load("./mt_results/mr1_mr2_mt.Rda")
 p_pet_eval_grid <- eval_grid
 
+
+# # "data_gof" save the goodness of fit results for ranking the models;
+# the results are stored in "model_order"
 data_gof <- eval_grid %>%
   select(region, season, iter, gof_result) %>%
   unnest(gof_result)
@@ -51,74 +54,96 @@ model_order <- data_gof %>%
 
 # pre-processing ----------------------------------------------------------
 
-get_consistent_rate <- function(x) {
+# The assessment results are labeled with numbers; consistent results are labeled with 4
+get_consistency_rate <- function(x) {
   (sum(x == 4)) / length(x)
 }
 
-get_consistent_rate_p_pet <- function(x1, x2) {
+# if both assessment are consistent,then the sum of the two assessments are 8
+get_consistency_rate_p_pet <- function(x1, x2) {
   x <- x1+x2
   (sum(x == 8)) / length(x)
 }
 
-
-# pet
-pet_eval_grid <- pet_eval_grid %>%
-  mutate(test_results = vector("list", 1))
-
-    # iterate over region, season, models
-for (i in 1:nrow(pet_eval_grid)) {
-  mt_tes <- pet_eval_grid$mt_tes[[i]]
-  test_results <- tibble(model = names(mt_tes),
-                         test_result = -9999)
-  
-  # iteration over machine learning methods
-  for (j in seq_along(mt_tes)) {
-    test_results$test_result[[j]] <- mt_tes[[j]] %>% unlist() %>% get_consistent_rate()
-  }
-  
-  pet_eval_grid$test_results[[i]] <- test_results
-}
-
-pet <- pet_eval_grid %>%
-  select(region, season, iter, test_results) %>%
-  unnest(test_results) %>%
-  dplyr::rename(mr2 = test_result)
-
-
-# p
+# Process the data for MR1
+#
+# create a column to store the average consistency rate for an outer CV iteration
 p_eval_grid <- p_eval_grid %>%
   mutate(test_results = vector("list", 1))
 
 # iterate over region, season, models
 for (i in 1:nrow(p_eval_grid)) {
+  # extract assessment results associated with the test set
   mt_tes <- p_eval_grid$mt_tes[[i]]
+  
+  # create a tibble to store the result for different models
   test_results <- tibble(model = names(mt_tes),
                          test_result = -9999)
   
   # iteration over machine learning methods
   for (j in seq_along(mt_tes)) {
-    test_results$test_result[[j]] <- mt_tes[[j]] %>% unlist() %>% get_consistent_rate()
+    test_results$test_result[[j]] <-
+      mt_tes[[j]] %>% unlist() %>% get_consistency_rate()
   }
   
+  # store the store for the current iteration
   p_eval_grid$test_results[[i]] <- test_results
 }
 
+# combine all the result into a tibble "p"
 p <- p_eval_grid %>%
   select(region, season, iter, test_results) %>%
   unnest(test_results) %>%
   dplyr::rename(mr1 = test_result)
 
 
-# p and PET
+# Process the data for MR2
+#
+# create a column to store the average consistency rate for an outer CV iteration
+pet_eval_grid <- pet_eval_grid %>%
+  mutate(test_results = vector("list", 1))
+
+# iterate over region, season, models
+for (i in 1:nrow(pet_eval_grid)) {
+  # extract assessment results associated with the test set
+  mt_tes <- pet_eval_grid$mt_tes[[i]]
+  
+  # create a tibble to store the result for different models
+  test_results <- tibble(model = names(mt_tes),
+                         test_result = -9999)
+  
+  # iteration over machine learning methods
+  for (j in seq_along(mt_tes)) {
+    test_results$test_result[[j]] <-
+      mt_tes[[j]] %>% unlist() %>% get_consistency_rate()
+  }
+  
+  # store the store for the current iteration
+  pet_eval_grid$test_results[[i]] <- test_results
+}
+
+# combine all the result into a tibble "pet"
+pet <- pet_eval_grid %>%
+  select(region, season, iter, test_results) %>%
+  unnest(test_results) %>%
+  dplyr::rename(mr2 = test_result)
+
+# Process the data for assessments using both MR1 and MR2
+#
+# create a column to store the average consistency rate for an outer CV iteration
 p_pet_grid <- p_pet_eval_grid %>%
   mutate(test_results = vector("list", 1))
 
 # iterate over region, season, models
 for (i in 1:nrow(p_pet_eval_grid)) {
-  
+  # extract assessment results associated with the test set
+  # "mt_tes_p_pet" stores the results when both MR1 and MR2 are used in the assessment; 
+  #  MR3 is used to further modify the inputs, which have already been modified by MR1
+  # "mt_tes_p" stores the results when MR1 is used in assessment 
   mt_tes_p_pet <- p_pet_eval_grid$mt_tes[[i]]
   mt_tes_p <- p_eval_grid$mt_tes[[i]]
   
+  # create a tibble to store the result for different models
   test_results <- tibble(model = names(mt_tes),
                          test_result = -9999)
   
@@ -127,12 +152,14 @@ for (i in 1:nrow(p_pet_eval_grid)) {
     x1 <- mt_tes_p_pet[[j]] %>% unlist()
     x2 <- mt_tes_p[[j]] %>% unlist()
     
-    test_results$test_result[[j]] <- get_consistent_rate_p_pet(x1, x2)
+    test_results$test_result[[j]] <- get_consistency_rate_p_pet(x1, x2)
   }
   
+  # store the store for the current iteration
   p_pet_grid$test_results[[i]] <- test_results
 }
 
+# combine all the result into a tibble "p_pet"
 p_pet <- p_pet_grid %>%
   select(region, season, iter, test_results) %>%
   unnest(test_results) %>%
@@ -141,6 +168,8 @@ p_pet <- p_pet_grid %>%
 
 # Plot --------------------------------------------------------------------
 
+# Join the assessment results from all three assessment experiments;
+# format the labels, and arranging the results
 data_plot <- p %>%
   left_join(pet,  by = c("region", "season", "iter", "model")) %>%
   left_join(p_pet,  by = c("region", "season", "iter", "model")) %>%
@@ -166,11 +195,14 @@ data_plot <- p %>%
     model = factor(model, levels = model_order)
   )
 
+# create the labs for piloting
 lab <- c(expression(MR[1]),
          expression(MR[2]),
          expression(MR[1] * ' & M' * R[2]))
 
 
+
+# Plotting the results for summer floods ----------------------------------
 
 ggplot(data_plot %>% filter(season == "Summer"), aes(item, value)) +
   geom_point(aes(color = item, shape = item),
@@ -211,8 +243,8 @@ ggsave(
   dpi = 600
 )
 
-# Plot supporting figure --------------------------------------------------
 
+# Plotting results on winter floods ---------------------------------------
 
 ggplot(data_plot %>% filter(season == "Winter"), aes(item, value)) +
   geom_point(aes(color = item, shape = item),
